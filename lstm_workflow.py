b8964c659af1ae782d0a1db383df9d1a9a155fdf
Michal Harakal <michal.harakal@googlemail.com>
2020-11-30 15:07:18 +0100

    Add scaffold files for LSTM based pipelines

---
+++ lstm_workflow.py
@@ -0,0 +1,38 @@
+def preprocess():
+    """
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def train():
+    """
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def evaluate():
+    """
+
+    Returns
+    -------
+
+    """
+    pass
+
+
+def main():
+    preprocess()
+    train()
+    evaluate()
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/lstm.py
@@ -0,0 +1,11 @@
+def build_classifier(classifier_name):
+    pass
+
+
+def main():
+    classifier_name = "LSTM"
+    build_classifier(classifier_name)
+
+
+if __name__ == '__main__':
+    main()
