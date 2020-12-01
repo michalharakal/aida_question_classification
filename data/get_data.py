e9437be0dadcbc59bfeb8ccebd6d0eb76530a910
Michal Harakal <>
2020-12-01 10:15:00 +0100

    Add prechecks for existence of local copy of data files. If they are missing, download them.

--- data/get_data.py
+++ data/get_data.py
@@ -1,10 +1,38 @@
-
-import re
 import pathlib
 import pandas as pd
+import os
+import shutil
+import requests
+
+
+def download_file(url, dest_folder):
+    local_filename = url.split('/')[-1] + ".txt"
+    path = os.path.join(dest_folder, local_filename)
+    with requests.get(url, stream=True) as r:
+        with open(path, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+    return local_filename
+
+
+def local_data_exists(file_name):
+    return os.path.isfile(file_name)
+
+
+def local_data_missing(file_name):
+    return not local_data_exists(file_name)
+
+
+def check_local_data():
+    """ Check existence of file in local directr and if they are missing, download them """
+    if local_data_missing('train_5500.label.txt') or local_data_missing('train_5500.label.txt'):
+        download_file("https://cogcomp.seas.upenn.edu/Data/QA/QC/train_5500.label", "./")
+        download_file("https://cogcomp.seas.upenn.edu/Data/QA/QC/TREC_10.label", "./")
+
 
 def process_question(row):
-   return " ".join(row.split(" ")[1:])
+    return " ".join(row.split(" ")[1:])
+
 
 def process_pd_input(file_name):
     path = pathlib.Path.cwd().joinpath('data', file_name)
@@ -17,18 +45,29 @@ def process_pd_input(file_name):
     return df
 
 
-def get_train_data(train_file = 'train_5500.label.txt'):
+def get_train_data(train_file='train_5500.label.txt'):
     """
 
     :param train_file:
     :return:
     """
+    if local_data_missing(train_file):
+        download_file("https://cogcomp.seas.upenn.edu/Data/QA/QC/train_5500.label", "./data")
+
     return process_pd_input(train_file)
 
-def get_test_data(test_file = 'TREC_10.label.txt'):
+
+def get_test_data(test_file='TREC_10.label.txt'):
     """
 
     :param test_file:
     :return:
     """
+    if local_data_missing(test_file):
+        download_file("https://cogcomp.seas.upenn.edu/Data/QA/QC/TREC_10.label", "./data")
+
     return process_pd_input(test_file)
+
+
+if __name__ == '__main__':
+    check_local_data()
