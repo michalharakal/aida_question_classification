e16c038e2c594ece80a1d9564c2b4bd361b591f3
Petra <pewos42@gmail.com>
2020-12-02 18:16:16 +0100

    correction get_data.py file_na+me is file_namn again

--- data/get_data.py
+++ data/get_data.py
@@ -26,8 +26,10 @@ def local_data_missing(file_name):
 def check_local_data():
     """ Check existence of file in local directr and if they are missing, download them """
     if local_data_missing('train_5500.label.txt') or local_data_missing('train_5500.label.txt'):
-        download_file("https://cogcomp.seas.upenn.edu/Data/QA/QC/train_5500.label", "./")
-        download_file("https://cogcomp.seas.upenn.edu/Data/QA/QC/TREC_10.label", "./")
+        download_file(
+            "https://cogcomp.seas.upenn.edu/Data/QA/QC/train_5500.label", "./")
+        download_file(
+            "https://cogcomp.seas.upenn.edu/Data/QA/QC/TREC_10.label", "./")
 
 
 def process_question(row):
@@ -53,7 +55,8 @@ def process_pd_input(file_name):
     df.columns = ["raw"]
     df['cat_combined'] = df.apply(lambda row: row["raw"].split(" ")[0], axis=1)
     df['category'] = df.apply(lambda row: row["raw"].split(":")[0], axis=1)
-    df['subcategory'] = df.apply(lambda row: row["raw"].split(" ")[0].split(":")[1], axis=1)
+    df['subcategory'] = df.apply(lambda row: row["raw"].split(" ")[
+                                 0].split(":")[1], axis=1)
     df['question'] = df.apply(lambda row: process_question(row["raw"]), axis=1)
 
     return df
@@ -68,7 +71,8 @@ def get_train_data(train_file='train_5500.label.txt'):
     :return: train data frame
     """
     if local_data_missing(train_file):
-        download_file("https://cogcomp.seas.upenn.edu/Data/QA/QC/train_5500.label", "./data")
+        download_file(
+            "https://cogcomp.seas.upenn.edu/Data/QA/QC/train_5500.label", "./data")
 
     return process_pd_input(train_file)
 
@@ -82,7 +86,8 @@ def get_test_data(test_file='TREC_10.label.txt'):
     :return: test data frame
     """
     if local_data_missing(test_file):
-        download_file("https://cogcomp.seas.upenn.edu/Data/QA/QC/TREC_10.label", "./data")
+        download_file(
+            "https://cogcomp.seas.upenn.edu/Data/QA/QC/TREC_10.label", "./data")
 
     return process_pd_input(test_file)
 
@@ -93,6 +98,11 @@ def main():
     """
     df = process_pd_input('TREC_10.label.txt')
     # print(df['cat_combined'])
+
+
+if __name__ == '__main__':
+    check_local_data()
+    # main()
 
 
 if __name__ == '__main__':
