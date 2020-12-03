2d9e7eeb9dd6c3e5d60a6712d5bf0588840d9ea4
Sven <s.wontroba@gmail.com>
2020-12-03 10:24:01 +0100

    play with diff ngram ranges

--- ngrams_workflow.py
+++ ngrams_workflow.py
@@ -37,7 +37,7 @@ def ng_CountVectorizer(X_train, ngram_range=(1, 3)):
     -------
 
     """
-    print(ngram_range)
+    print('ngram_range=', ngram_range)
     count_vectorizer = CountVectorizer(stop_words=None, ngram_range=ngram_range )
     bag_of_words = count_vectorizer.fit_transform(X_train)
 
@@ -71,22 +71,32 @@ def main():
     df_test = data.get_test_data()
     categories = df_train.category.unique()
 
+
+
     print(categories)
     X_train_desc = df_train[df_train.category == 'DESC' ]['question']
     X_train = df_train['question']
 
-    count_vectorizer, df_bag_of_words, bag_of_words = ng_CountVectorizer(X_train_desc, (1, 2))
-    vocabulary = count_vectorizer.vocabulary_
+    count_vectorizer, df_bag_of_words, bag_of_words = ng_CountVectorizer(X_train_desc, (2, 3))
+
+
     # sort the vocab
-    #sorted_items = sort_vocab(bag_of_words[0].tocoo())
+    # sorted_items = sort_vocab(bag_of_words[0].tocoo())
     # Get feature names (words/n-grams). It is sorted by position in sparse matrix
-    feature_names = count_vectorizer.get_feature_names()
-    #n_grams = extract_topn_from_vector(feature_names, sorted_items, 10)
+    # feature_names = count_vectorizer.get_feature_names()
+    # n_grams = extract_topn_from_vector(feature_names, sorted_items, 10)
+
+    # doch anders.....
+
+    # Get full vocab names
+    vocabulary = count_vectorizer.vocabulary_
 
     max_words = dict((word, df_bag_of_words[word].sum()) for word in vocabulary)
     smax_words = sorted(max_words, key=max_words.get, reverse=True)[:10]
+
     print(smax_words)
-    print(max_words['what'])
+    for word in smax_words:
+        print( word, ':', max_words[word])
 
 
     print()
