8846d10451296d34c319993ee3211851d7ff1b35
Sven <s.wontroba@gmail.com>
2020-12-03 19:19:21 +0100

    results to csv for f1 report in fkt pipeline_results

--- svm_workflow.py
+++ svm_workflow.py
@@ -35,19 +35,19 @@ def preprocess(df_train, df_test):
 
     return df_train, df_test
 
-def preprocess_stpw_clean_lem(df_train, df_test):
 
+def preprocess_stpw_clean_lem(df_train, df_test):
     df_train = txtm.preprocess_dataframe(df_train)
     df_test = txtm.preprocess_dataframe(df_test)
 
     return df_train, df_test
 
 
-
-def pipeline_results(pipeline, X_test, y_test):
+def pipeline_results(pipeline, X_test, y_test, name='pipe'):
     """
     print pipeline results for given pipeline and X, y values
 
+    :param name:
     :param pipeline: fitted sklearn pipeline
     :param X_test: X_test values
     :param y_test: y_test values
@@ -58,8 +58,11 @@ def pipeline_results(pipeline, X_test, y_test):
 
     print('accuracy_score: CountVectorized', accuracy_score(y_test, y_pred_pipeline))
 
-    report = classification_report(y_test, y_pred_pipeline)
-    print(report)
+    report = classification_report(y_test, y_pred_pipeline, output_dict=True)
+    df = pd.DataFrame(report).transpose()
+    print(df)
+    # save df results to cvs for later report
+    df.to_csv( './report/' + name + '.csv')
 
     val_confusion_matrix = confusion_matrix(y_test, y_pred_pipeline)
     print(f'Confusion Matrix: \n{val_confusion_matrix}')
@@ -71,34 +74,28 @@ def main():
     df_test = data.get_test_data()
 
     # preprocess questions regex clean, stopwords, lemmatizer
-    df_train, df_test = preprocess(df_train, df_test)
+    # df_train, df_test = preprocess(df_train, df_test)
 
     df_train, df_test = preprocess_stpw_clean_lem(df_train, df_test)
 
     # defining main test and train data of main categories
-    X_train = df_train['text_lemma'] # question - data regex, stopwords, lem
-    y_train = df_train['category']  # using main category
-    X_test = df_test['text_lemma'] # question - data not cleaned
-    y_test = df_test['category']  # using main category
+    # df col with text and cat/subcategory to be used
+    df_col = 'text_stopwords'
+    # category = 'subcategory'
+    category = 'category'
 
-    # Test - defining main test and train data of main sub categories
-    X_train = df_train.question
-    y_train = df_train['category']  # using main category
-    X_test = df_test.question
-    y_test = df_test['category']  # using main category
+    X_train = df_train[df_col]  # question - data regex, stopwords, lem
+    y_train = df_train[category]  # using main category
+    X_test = df_test[df_col]  # question - data not cleaned
+    y_test = df_test[category]  # using main category
 
-    # Test - defining main test and train data of main sub categories
-    # X_train = df_train.question
-    # y_train = df_train['subcategory']  # using main category
-    # X_test = df_test.question
-    # y_test = df_test['subcategory']  # using main category
 
     ##############################
     # prediction
 
     pipe_tf = Pipeline(steps=[
         ('data_vec', TfidfVectorizer()),
-        ('model', svm.LinearSVC())  # C=10.0
+        ('model', svm.LinearSVC())  # C=1.0
     ])
     print('cross_val_score, TfidfVectorized :', cross_val_score(pipe_tf, X_train, y_train).mean())
 
@@ -106,13 +103,15 @@ def main():
         ('data_cv', CountVectorizer(stop_words=[])),
         ('model', svm.LinearSVC())
     ])
-    print('cross_val_score, CountVectorized  pipe_cv_no_stop_words: default ngram_range=(1, 1)', cross_val_score(pipe_cv, X_train, y_train).mean())
+    print('cross_val_score, CountVectorized  pipe_cv_no_stop_words: default ngram_range=(1, 1)',
+          cross_val_score(pipe_cv, X_train, y_train).mean())
 
     pipe_cv_ng12 = Pipeline(steps=[
         ('data_cv', CountVectorizer(stop_words=[], ngram_range=(1, 2))),
         ('model', svm.LinearSVC())
     ])
-    print('cross_val_score, pipe_cv_no_stop_words ngram_range=(1, 2) :', cross_val_score(pipe_cv_ng12, X_train, y_train).mean())
+    print('cross_val_score, pipe_cv_no_stop_words ngram_range=(1, 2) :',
+          cross_val_score(pipe_cv_ng12, X_train, y_train).mean())
 
     pipe_cv_ng13 = Pipeline(steps=[
         ('data_cv', CountVectorizer(stop_words=[], ngram_range=(1, 3))),
@@ -135,33 +134,45 @@ def main():
     print('cross_val_score, pipe_cv_ng23 ngram_range=(2, 3) :',
           cross_val_score(pipe_cv_ng23, X_train, y_train).mean())
 
+    pipe_cv_ng33 = Pipeline(steps=[
+        ('data_cv', CountVectorizer(stop_words=[], ngram_range=(3, 3))),
+        ('model', svm.LinearSVC())
+    ])
+    print('cross_val_score, pipe_cv_ng33 ngram_range=(3, 3) :',
+          cross_val_score(pipe_cv_ng33, X_train, y_train).mean())
+
     ##############################
     # Measuring the performance
     # Testing the pipeline models using Count Vectorized and again with Tfi-df Vectorized.
 
     pipe_tf.fit(X_train, y_train)
     print('results TfidfVectorized pipeline fkt:')
-    pipeline_results(pipe_tf, X_test, y_test)
+    pipeline_results(pipe_tf, X_test, y_test, df_col + '_pipe_TfidfVect')
 
     pipe_cv.fit(X_train, y_train)
     print('results CountVectorized pipeline fkt:')
-    pipeline_results(pipe_cv, X_test, y_test)
+    pipeline_results(pipe_cv, X_test, y_test, df_col + '_pipe_cv')
 
     pipe_cv_ng12.fit(X_train, y_train)
     print('results pipe_cv_no_stop_words ngram_range=(1, 2) fkt:')
-    pipeline_results(pipe_cv_ng12, X_test, y_test)
+    pipeline_results(pipe_cv_ng12, X_test, y_test, df_col + '_pipe_cv_ng12')
 
     pipe_cv_ng13.fit(X_train, y_train)
     print('results pipe_cv_no_stop_words ngram_range=(1, 3) fkt:')
-    pipeline_results(pipe_cv_ng13, X_test, y_test)
+    pipeline_results(pipe_cv_ng13, X_test, y_test, df_col + '_pipe_cv_ng13')
 
     pipe_cv_ng22.fit(X_train, y_train)
     print('results pipe_cv_ng22 ngram_range=(2, 2) fkt:')
-    pipeline_results(pipe_cv_ng22, X_test, y_test)
+    pipeline_results(pipe_cv_ng22, X_test, y_test, df_col + '_pipe_cv_ng22')
 
     pipe_cv_ng23.fit(X_train, y_train)
     print('results pipe_cv_ng23 ngram_range=(2, 3) fkt:')
-    pipeline_results(pipe_cv_ng23, X_test, y_test)
+    pipeline_results(pipe_cv_ng23, X_test, y_test, df_col + '_pipe_cv_ng23')
+
+    pipe_cv_ng33.fit(X_train, y_train)
+    print('results pipe_cv_ng33 ngram_range=(3, 3) fkt:')
+    pipeline_results(pipe_cv_ng33, X_test, y_test, df_col + '_pipe_cv_ng33')
+
 
 if __name__ == '__main__':
     main()
