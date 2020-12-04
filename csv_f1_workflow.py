388b23b655879c30a65c10cb854577b76ca3b900
Sven <s.wontroba@gmail.com>
2020-12-04 11:50:40 +0100

    bug: fix split and pivot_table

--- csv_f1_workflow.py
+++ csv_f1_workflow.py
@@ -9,15 +9,21 @@ import os
 
 files = glob.glob('report/question_*.csv')
 
-df = pd.concat([pd.read_csv(fp).assign(filename=os.path.basename(fp).split('.')[0]) for fp in files])
+df = pd.concat([pd.read_csv(fp).assign(filename=os.path.basename(fp).split(',')[0]) for fp in files])
+
+print(df.columns)
+
 df.columns = ['category', 'precision', 'recall', 'f1', 'support', 'filename']
 
 print(df)
 
 
 #%%
+
 df[['category', 'f1', 'filename']]
 
-df_pv = df.pivot_table(columns=['filename'])
+df_pv = df.pivot_table(columns=['filename'], index=['category']).round(2)
 
 print(df_pv)
+
+
