918bb251d2746bbf85ef3fa2d50a8d3d68aa10a6
Sven <s.wontroba@gmail.com>
2020-12-04 11:30:00 +0100

    pivot_table

--- csv_f1_workflow.py
+++ csv_f1_workflow.py
@@ -6,13 +6,18 @@ import os
 
 #%%
 
-# df = pd.concat(map(pd.read_csv, glob.glob('report/question_*.csv')))
-#df = pd.read_csv('report/question_pipe_cv.csv', )
 
 files = glob.glob('report/question_*.csv')
 
-df = pd.concat([pd.read_csv(fp).assign(New=os.path.basename(fp).split('.')[0]) for fp in files])
+df = pd.concat([pd.read_csv(fp).assign(filename=os.path.basename(fp).split('.')[0]) for fp in files])
+df.columns = ['category', 'precision', 'recall', 'f1', 'support', 'filename']
 
 print(df)
 
+
 #%%
+df[['category', 'f1', 'filename']]
+
+df_pv = df.pivot_table(columns=['filename'])
+
+print(df_pv)
